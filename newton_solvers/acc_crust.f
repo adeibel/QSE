9dfd9ac5f3226e15d00b08705de58ec8bc2e3abd
Alex Deibel <deibelal@msu.edu>
2014-08-28 11:39:38 -0400

    added a driver program for accreted composition

---
+++ newton_solvers/acc_crust.f
@@ -0,0 +1,24 @@
+	 """
+     driver program for accreted crust composition solver
+     - follows composition as it is pushed through the inner crust
+     - uses Newton-Raphson solver to calculate composition in inner 
+       crust 
+  	 """
+      program acc_crust
+
+      use qse_solver              
+      use const_def
+      use const_lib
+      use num_def
+      use mtx_lib
+      use mtx_def
+      
+      implicit none
+      logical, parameter :: show_all = .false.  ! false for releases
+      integer ::  k, solver, omp_get_thread_num
+      logical :: m_band, j_band, quiet
+
+	  call follow_outer_crust_composition
+      call follow_inner_crust_composition
+
+      end program acc_crust
