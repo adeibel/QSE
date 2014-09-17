73a02cf6e82a9e80798f6e6745e0cb63bcb7607d
Alex Deibel <deibelal@msu.edu>
2014-09-17 15:06:27 -0400

    added an ash_table_support module for inner crust
    need to select variables only on if the nucleus is abundant or not

--- support_modules/dist_table_support.f
+++ support_modules/dist_table_support.f
@@ -1,46 +1,39 @@
 module dist_table
 	! storage for the mass table
 	type dist_table_type
-		integer :: Ntable	! number of table entries
-		!integer :: Zmin, Zmax	! minimum, maximum Z
-		integer :: Pmin, Pmax
-		real, dimension(:), pointer :: P    ! dist coordinate
-		real, dimension(:), pointer :: mu_e ! electron chemical potential
-		real, dimension(:), pointer :: mu_n ! neutron chemical potential
-		integer, dimension(:), pointer :: Z, A
-		real, dimension(:), pointer :: abun
-		! bookmarks
+		integer :: Zmin, Zmax	! minimum, maximum Z
 		integer :: Nelements	! = Zmax-Zmin+1
-		!index of first nuclide of a given element
-		integer, dimension(:), pointer :: Pstart	! (Nelements) 
-		!real, dimension(:), pointer :: Pstart		
-		! minimum, maximum neutron no. for each element
-		!integer, dimension(:), pointer :: mu_n_min, mu_n_max	! (Nelements)
-		real, dimension(:), pointer :: mu_n_min, mu_n_max	
+		integer, dimension(:), pointer :: Zstart	! (Nelements) 
+		integer, dimension(:), pointer :: Nmin, Nmax	! (Nelements)		
+		integer :: Ntable	! number of table entries
+		integer, dimension(:), pointer :: Z, N, A 
+		real, dimension(:), pointer :: BE !binding energy 
+		real, dimension(:), pointer :: Y !abundance fraction 
+		real :: P !pressure
 	end type dist_table_type
 	logical, save :: dist_table_is_loaded = .FALSE.
 
 	type(dist_table_type), target :: winvn_dist_table
 
- contains
+contains
 
 	subroutine dist_table_shutdown()
 		type(dist_table_type), pointer :: dt
 		if (.not.dist_table_is_loaded) return
 		dt => winvn_dist_table
-		deallocate(dt% Pstart)
-		deallocate(dt% P)
-		deallocate(dt% mu_n_min)
-		deallocate(dt% mu_n_max)
-		deallocate(dt% mu_e)
-		deallocate(dt% mu_n)
+		deallocate(dt% Zstart)
+		deallocate(dt% Nmin)
+		deallocate(dt% Nmax)
 		deallocate(dt% Z)
+		deallocate(dt% N)
 		deallocate(dt% A)
-		deallocate(dt% abun)
+		deallocate(dt% BE)
+		deallocate(dt% Y)	
 		dt% Ntable = 0
-		dt% Pmin = -1
-		dt% Pmax = -1
+		dt% Zmin = -1
+		dt% Zmax = -1		
 		dt% Nelements = 0
+		dist_table_is_loaded = .FALSE.
 	end subroutine dist_table_shutdown
 
 	subroutine load_dist_table(datadir,datafile,ierr)
@@ -52,7 +45,7 @@ module dist_table
 		character(len=256) :: filename
 		type(dist_table_type), pointer :: dt
 		integer :: i, Ntab, Nele, iounit
-		integer :: currentP, id_el
+		integer :: currentZ, id_el
 
 		if (dist_table_is_loaded) then
 			ierr = 1
@@ -72,8 +65,10 @@ module dist_table
 			return
 		end if
 
-		! read through the file to get number of entries
+		! read through the file to get number of entries (skips first three lines)
 		Ntab = 0
+		read(iounit,*,iostat=ierr)
+		read(iounit,*,iostat=ierr)
 		read(iounit,*,iostat=ierr)
 		if (ierr /= 0) then
 			call alert(ierr,'dist_table_support: load_dist_table:: unable to read any lines')
@@ -99,21 +94,23 @@ module dist_table
 		
 		ierr = 0
 		dt => winvn_dist_table
-	
 		dt% Ntable = Ntab
-		! allocate the tables
-		allocate(dt% P(Ntab), dt% mu_e(Ntab), dt% mu_n(Ntab), &
-			&	 dt% Z(Nele), dt% A(Nele), dt% abun(Nele))
 
-		! now read in the table, skipping first line
+		! allocate the tables		
+		allocate(dt% Z(Ntab), dt% N(Ntab), dt% A(Ntab), dt% BE(Ntab), dt% Y(Ntab), &
+			&		dt% Zstart(Ntab), dt% Nmin(Ntab), dt% Nmax(Ntab))
+		
+		! now read in the table, skipping first three lines
 		read(iounit,*,iostat=ierr)
+		read(iounit,*,iostat=ierr) 
+		read(iounit,*,iostat=ierr) dt% P
 		if (ierr /= 0) then
 			call alert(ierr,'dist_table_support: load_dist_table:: unable to read any lines')
 			return
 		end if
 		
 		do i = 1, dt% Ntable
-			read(iounit,*,iostat=ierr) dt% P(i), dt% mu_e(i), dt% mu_n(i)
+			read(iounit,*,iostat=ierr) 	dt% Z(i), dt% N(i), dt% A(i), dt% BE(i), dt% Y(i) 
 			if (ierr /=0) then
 			   call alert(ierr,'dist_table_support: load_dist_table:: unable to read lines')
 			   exit
@@ -122,57 +119,8 @@ module dist_table
 		close(iounit)
 		call free_iounit(iounit)
 
-!fix following
-
-		! set up the bookmarks; table is assumed to be sorted!
-		dt% Pmin = 1	   !minval(dt% P)
-		dt% Pmax = dt% Ntable  !maxval(dt% P)
-		
-		! allocate the bookmarks
-		Nele = dt% Pmax- dt% Pmin+1
-		dt% Nelements = Nele
-		allocate(dt% Pstart(Nele), dt% mu_n_min(Nele), dt% mu_n_max(Nele))
-		
-		!currentP = -1
-	    currentP = 0.
-		id_el = 0
-		do i = 1, dt% Ntable
-			if (dt% P(i) /= currentP) then	! found a new dist
-				id_el = id_el + 1
-				dt% Pstart(id_el) = i   ! Pstart can be an integer?
-				dt% mu_n_min(id_el) = dt% mu_n(i)
-				if (id_el > 1) then
-					dt% mu_n_max(id_el-1) = dt% mu_n(i-1)
-				end if
-				currentP = dt% P(i)
-			end if
-		end do
-		dt% mu_n_max(id_el) = dt% mu_n(dt% Ntable)
 
 		if (ierr == 0) dist_table_is_loaded = .TRUE.
 	end subroutine load_dist_table
-
-	function dist_table_index(P, mu_n, ierr) result(id)
-		real, intent(in) :: P, mu_n
-		integer, intent(out) :: ierr
-		integer :: id
-		integer :: Pindex
-		type(dist_table_type), pointer :: dt
-	
-		dt => winvn_dist_table
-		ierr = 0
-		id = -1
-		if (P < dt% Pmin .or.  P > dt% Pmax) then
-			ierr = -1
-			return
-		end if
-		Pindex = P - dt% Pmin + 1
-		if (mu_n < dt% mu_n_min(Pindex) .or. mu_n > dt% mu_n_max(Pindex)) then
-			ierr = -2
-			return
-		end if
-	
-		id = dt% Pstart(Pindex) + mu_n - dt% mu_n_min(Pindex)
-	end function dist_table_index
 
 end module dist_table
